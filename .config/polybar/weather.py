e36fb36fe353dc8c732cbff8d60ab71def0a624b
Sopwai <Sopwai@users.noreply.github.com>
2017-10-23 20:45:38 -0500

    error handling when w/o wifi

--- .config/polybar/weather.py
+++ .config/polybar/weather.py
@@ -9,7 +9,10 @@ api_key = ""                              #get an api key from openweathermap
 units = ""                                  #Metric or Imperial units
 unit_key = ""                              #F C or K, i think kelvin is available
 
-weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".format(city, api_key, units)).read())[2:-1])
+try:
+  weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".format(city, api_key, units)).read())[2:-1])
+except:
+  print("Weather is not available at this time. Check your internet connection.")
 
 info = weather["weather"][0]["description"].capitalize()
 temp = int(float(weather["main"]["temp"]))
