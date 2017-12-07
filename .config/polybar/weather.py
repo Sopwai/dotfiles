5aead65ea3c6c9759961e8ee5e0b7fed14837b32
Sopwai <Sopwai@users.noreply.github.com>
2017-12-07 00:30:34 -0600

    my own error msg pissed me off, fixed it

--- .config/polybar/weather.py
+++ .config/polybar/weather.py
@@ -13,7 +13,7 @@ unit_key = ""                              #F C or K, i think kelvin is availabl
 try:
   weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".format(city, api_key, units)).read())[2:-1])
 except:
-  print("Weather is not available at this time. Check your internet connection.")
+  print("oops lol")      # i'm the worst
 
 info = weather["weather"][0]["description"].capitalize()
 temp = int(float(weather["main"]["temp"]))
