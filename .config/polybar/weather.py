e1b32d4f68363d0500b382e4884f6d4ddb58b09a
Sopwai <Sopwai@users.noreply.github.com>
2017-12-06 20:30:50 -0600

    Update weather.py
    added comment

--- .config/polybar/weather.py
+++ .config/polybar/weather.py
@@ -9,6 +9,7 @@ api_key = ""                              #get an api key from openweathermap
 units = ""                                  #Metric or Imperial units
 unit_key = ""                              #F C or K, i think kelvin is available
 
+#this could be done better but for now it keeps me from pulling my hair out
 try:
   weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".format(city, api_key, units)).read())[2:-1])
 except:
